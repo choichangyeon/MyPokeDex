f9fc832d8c8ff88570fa9c09d0364fe767dc1aed
choichangyeon <changyon99@naver.com>
2025-02-03 19:58:46 +0900

    feat/#1-router 버튼 구현

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Dex from "./Dex";
-import Details from "./Details";
+import Home from "./pages/Home";
+import Dex from "./pages/Dex";
+import Details from "./pages/Details";
 
 const Router = () => {
   return (
---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PokemonCard from "./PokemonCard";
+
+const PokemonList = () => {
+  return <PokemonCard></PokemonCard>;
+};
+
+export default PokemonList;
