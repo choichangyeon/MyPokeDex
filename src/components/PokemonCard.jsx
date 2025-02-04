47695a7998f30676473b250fecf00fb1e90e08e2
choichangyeon <changyon99@naver.com>
2025-02-04 12:16:32 +0900

    feat/#4-추가하기 버튼 생성

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,10 +3,15 @@ import styled from "styled-components";
 
 const PokemonCard = ({ pokemon }) => {
   const CardBox = styled.div`
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
     width: 160px;
     height: 220px;
     border: 1px solid black;
     border-radius: 10px;
+    margin: 10px 0 10px 0;
   `;
   const Img = styled.img`
     object-position: center;
@@ -14,15 +19,26 @@ const PokemonCard = ({ pokemon }) => {
   `;
   const Info = styled.div`
     width: 100px;
+    text-align: center;
+  `;
+  const AddBtn = styled.button`
+    background-color: green;
   `;
   return (
     <CardBox>
       <Img src={pokemon.img_url} alt="Pokemon" />
       <Info>
-        {pokemon.id}, {pokemon.korean_name}
+        NO.{pokemon.id.toString().padStart(3, "0")}
+        <br />
+        {pokemon.korean_name}
       </Info>
+      <AddBtn onClick={testBtn} value={pokemon.id}>
+        추가하기
+      </AddBtn>
     </CardBox>
   );
 };
+
+const testBtn = (e) => {};
 
 export default PokemonCard;
