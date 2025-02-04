5505c5a40a5eb6211df934be95dd111bcce7a4be
choichangyeon <changyon99@naver.com>
2025-02-04 11:52:31 +0900

    feat/#4-포켓몬 리스트 레이아웃 수정

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import PokemonCard from "./PokemonCard";
 import MOCK_DATA from "../constants/MOCK_DATA";
+import styled from "styled-components";
 
 const PokemonList = () => {
   const pokemonList = MOCK_DATA;
+  const ListBox = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    flex-flow: row wrap;
+  `;
   return (
-    <>
+    <ListBox>
       {pokemonList.map((pokemon) => {
         return <PokemonCard pokemon={pokemon}></PokemonCard>;
       })}
-    </>
+    </ListBox>
   );
 };
 
