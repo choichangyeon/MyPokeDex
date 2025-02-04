a8ade7ae1ace4760cfdba900f5b73c1192f65f1f
choichangyeon <changyon99@naver.com>
2025-02-04 11:37:57 +0900

    feat/#4-기본 라이업 구성

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,55 @@
 import React from "react";
 import styled from "styled-components";
+import pokemon_ball from "/src/assets/PokemonBall.png";
 
-const Dashboard = () => {
+const Dashboard = ({ Lineup }) => {
   const BorderBox = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
     width: 100%;
     height: 200px;
     border: 2px blue solid;
+    margin-bottom: 20px;
   `;
+
+  const EmptyPokemon = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 140px;
+    height: 180px;
+    border: 1px dashed black;
+    border-radius: 10px;
+  `;
+
+  const PokemonBall = styled.img`
+    width: 80px;
+    object-position: center;
+    object-fit: cover;
+  `;
+
   return (
     <BorderBox>
-      <div>Dashboard</div>
+      <EmptyPokemon>
+        <PokemonBall src={pokemon_ball}></PokemonBall>
+      </EmptyPokemon>
+      <EmptyPokemon>
+        <PokemonBall src={pokemon_ball}></PokemonBall>
+      </EmptyPokemon>
+      <EmptyPokemon>
+        <PokemonBall src={pokemon_ball}></PokemonBall>
+      </EmptyPokemon>
+      <EmptyPokemon>
+        <PokemonBall src={pokemon_ball}></PokemonBall>
+      </EmptyPokemon>
+      <EmptyPokemon>
+        <PokemonBall src={pokemon_ball}></PokemonBall>
+      </EmptyPokemon>
+      <EmptyPokemon>
+        <PokemonBall src={pokemon_ball}></PokemonBall>
+      </EmptyPokemon>
     </BorderBox>
   );
 };
--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PokemonList from "../components/PokemonList";
 import Dashboard from "../components/Dashboard";
 
 const Dex = () => {
+  useEffect(select, Lineup);
   return (
     <>
-      <Dashboard></Dashboard>
+      <Dashboard Lineup={Lineup}></Dashboard>
       <PokemonList></PokemonList>;
     </>
   );
 };
+
+let Lineup = [];
+
+const select = () => {};
 
 export default Dex;
